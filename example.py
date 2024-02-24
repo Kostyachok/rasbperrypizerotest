066d73a5598c9c4e63d700bd2294b21cd7834036
Константин Колотилов <konstantinkolotilov@MacBook-Pro-Konstantin.local>
2024-02-24 11:52:09 +0300

    i2c example

--- example.py
+++ example.py
@@ -1,37 +1,5 @@
-import random
+from huskylib import HuskyLensLibrary
 
-def get_user_choice():
-    user_choice = input("Choose: rock, paper, or scissors: ").lower()
-    while user_choice not in ['rock', 'paper', 'scissors']:
-        print("Invalid choice. Please try again.")
-        user_choice = input("Choose: rock, paper, or scissors: ").lower()
-    return user_choice
+hl = HuskyLensLibrary("I2C","", address=0x32, channel=0)
 
-def get_computer_choice():
-    return random.choice(['rock', 'paper', 'scissors'])
-
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "It's a tie!"
-    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
-         (user_choice == 'scissors' and computer_choice == 'paper') or \
-         (user_choice == 'paper' and computer_choice == 'rock'):
-        return "You win!"
-    else:
-        return "Computer wins!"
-
-def play_game():
-    print("Welcome to the Rock, Paper, Scissors game!")
-    while True:
-        user_choice = get_user_choice()
-        computer_choice = get_computer_choice()
-        print("Your choice:", user_choice)
-        print("Computer's choice:", computer_choice)
-        print(determine_winner(user_choice, computer_choice))
-        play_again = input("Do you want to play again? (yes/no): ").lower()
-        if play_again != 'y':
-            print("Thanks for playing!")
-            break
-
-if __name__ == "__main__":
-    play_game()
+print(hl.knock())
